c582680b08d9a4503db56023708b7f3566cbe767
rossreillypitt <rossreilly@pitt.edu>
2022-09-22 17:34:06 -0400

    first commit

---
+++ hmda.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+flag = True
+hmda_cols = []
+ac_hmda = []
+with open("2021_lar.txt") as o:
+    for line in o:
+        while flag:
+            hmda_cols.append(line.strip())
+            hmda_cols = hmda_cols[0].strip("|")
+            flag = False
+        if line[41:46] == '42003':
+            ac_hmda.append(line.strip())
+    ac_hmda = [item.split("|") for item in ac_hmda]
+
+ac_hmda_dF = pd.DataFrame(ac_hmda, columns=hmda_cols)
+ac_hmda_dF.to_csv("ac_hmda.csv", index=False)
+#the end for now
