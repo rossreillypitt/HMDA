10f001a91954f3921048abcd383de50e54d50db4
rossreillypitt <rossreilly@pitt.edu>
2022-12-05 16:46:45 -0500

    strip to split in line 10

--- hmda.py
+++ hmda.py
@@ -7,7 +7,7 @@ with open("2021_lar.txt") as o:
     for line in o:
         while flag:
             hmda_cols.append(line.strip())
-            hmda_cols = hmda_cols[0].strip("|")
+            hmda_cols = hmda_cols[0].split("|")
             flag = False
         if line[41:46] == '42003':
             ac_hmda.append(line.strip())
@@ -16,3 +16,5 @@ with open("2021_lar.txt") as o:
 ac_hmda_dF = pd.DataFrame(ac_hmda, columns=hmda_cols)
 ac_hmda_dF.to_csv("ac_hmda.csv", index=False)
 #the end for now
+
+
